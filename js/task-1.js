00fb71c3fcf064e36e89c95dd5bc7d5580ef71eb
Serhii-Korsh <167204860+Serhii-Korsh@users.noreply.github.com>
2024-08-25 19:07:58 +0300

    changes hw07

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,9 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid;
-    if (customerCredits >= totalPrice) {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-} else {
-       return `Insufficient funds!`;
-}
-   
-};
-
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${categoryItems.length}`);
+categoryItems.forEach((item) => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const categoryElementsCount = item.querySelectorAll('ul li').length;
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElementsCount}`);
+});
--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,12 @@
-function checkForSpam(message) {
- const messagetoLower = message.toLowerCase()  
-if (messagetoLower.includes("spam")) {
-  return true
-} else if (messagetoLower.includes("sale")) {
-  return true
-} else {
- return false
-}
-};
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+textInput.addEventListener("input", (event) => {
+    const textInputValue = textInput.value.trim();
+    if (textInputValue === "") {
+      output.textContent = 'Anonymous';
+    } else {
+      output.textContent = event.currentTarget.value;
+    }
+  
+});
